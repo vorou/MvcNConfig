157dcb7612ab9ed1000f3d93829400b38b33725b
vorou <vorou@skbkontur.ru>
2014-04-02 09:49:14 +0600

    init

---
+++ MvcNConfig/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace MvcNConfig.Controllers
+{
+    public class HomeController : Controller
+    {
+        // compilation error
+        public ActionResult Index()
+        {
+            var appSetting = ConfigurationManager.AppSettings["name"];
+            return View((object) appSetting);
+        }
+
+        // works ok
+        public string ConfigName()
+        {
+            return ConfigurationManager.AppSettings["name"];
+        }
+    }
+}
---
+++ MvcNConfig/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using NConfig;
+
+namespace MvcNConfig
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            NConfigurator.UsingFile("Local.config").SetAsSystemDefault();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
